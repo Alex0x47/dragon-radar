8032d6e7c434b3fede2bc6d488947ed569f98070
Alex0x47 <grisey.alexandre@gmail.com>
2019-08-28 22:15:06 +0200

    create radar page, add seperate DB images, add dbs in radar view

--- constants/game.js
+++ constants/game.js
@@ -1,1 +1,38 @@
 export const RANGE = 500; //metres between user and farest DB
+export let DRAGON_BALLS = [
+    {
+        id: 1,
+        found: false,
+        image_path: require('../assets/images/ball1.png'),
+    },
+    {
+        id: 2,
+        found: false,
+        image_path: require('../assets/images/ball2.png'),
+    },
+    {
+        id: 3,
+        found: false,
+        image_path: require('../assets/images/ball3.png'),
+    },
+    {
+        id: 4,
+        found: false,
+        image_path: require('../assets/images/ball4.png'),
+    },
+    {
+        id: 5,
+        found: false,
+        image_path: require('../assets/images/ball5.png'),
+    },
+    {
+        id: 6,
+        found: false,
+        image_path: require('../assets/images/ball6.png'),
+    },
+    {
+        id: 7,
+        found: false,
+        image_path: require('../assets/images/ball7.png'),
+    },
+]
--- screens/Radar.js
+++ screens/Radar.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, Dimensions, Image } from 'react-native';
+const { width, height } = Dimensions.get('window');
+
+import { game } from '../constants';
+
+import { theme } from '../constants';
 
 import GeoService from '../services/geoService';
 
 export default class Radar extends Component {
 
     state = {
-        geoService: new GeoService()
+        geoService: new GeoService(),
+        dragon_balls: game.DRAGON_BALLS
     };
 
-    // componentDidMount(){
+    static navigationOptions = {
+        // headerStyle: {
+        //     backgroundColor: theme.COLORS.secondary,
+        // }
+        header: null
+        
+    }
 
-    // }
+    componentDidMount(){
+
+        //emulate a dragon ball the user find
+        let ballFound = this.state.dragon_balls.find(ball => ball.id == 3);
+        ballFound.found = true;
+
+        let dbFoundIndex = this.state.dragon_balls.findIndex(db => db.id == ballFound.id);
+        let newDB = this.state.dragon_balls;
+        newDB[dbFoundIndex] = ballFound;
+
+        setTimeout(() => {
+            this.setState({dragon_balls: newDB, ...this.state});
+        }, 3000);
+
+    }
 
     drawDragonBalls(){
 
@@ -30,10 +56,50 @@ export default class Radar extends Component {
 
     render(){
         return (
-            <View>
-                <Text>Dragon Radar - (Liste des dragons balls trouvées et non trouvées en opacité 0.7)</Text>
-                <Text>Radar avec background</Text>
-            </View>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.dragonlist}>
+                    {
+                        this.state.dragon_balls.map(ball => {
+                            return <Image key={ball.id} style={[styles.list_ball, ball.found ? styles.found_ball : null]} source={ball.image_path} />
+                        })
+                    }
+                </View>
+                <View style={styles.radar}>
+                    <Text>
+                        RADAR CONTENT
+                    </Text>
+                </View>
+            </SafeAreaView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: theme.COLORS.green_bg,
+        justifyContent: 'center'
+    },
+    dragonlist: { //parent
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: theme.COLORS.secondary,
+        alignItems: 'center', //horizontal align magic
+        justifyContent:'center' //vertical align magic
+    },
+    list_ball: {
+        width: 45,
+        height: 45,
+        margin: 4,
+        opacity: 0.5
+    },
+    found_ball: {
+        opacity: 1
+    },
+    text: {
+    },
+    radar: {
+        flex: 7,
+        alignItems: 'center'
+    }
+})
